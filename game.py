f6dbf244f21f758c7fd84dcfeae3c092311cd4f6
uraniarutherfordium <34132520+uraniarutherfordium@users.noreply.github.com>
2018-03-03 15:26:04 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,10 +1,10 @@
 import os
 from g import *
+import json
 
 pygame.init()
-size = width, height = 600, 600
+size = width, height = 800, 800
 screen = pygame.display.set_mode(size)
-screen.fill(pygame.Color('grey'))
 
 class Creature(pygame.sprite.Sprite):
     def __init__(self, kind, name, hp, mp, sp, inventory, tex, x, y, group):
@@ -13,10 +13,10 @@ class Creature(pygame.sprite.Sprite):
         self.hp, self.mp, self.sp = hp, mp, sp
         self.inventory = inventory
 
-        self.image = pygame.image.load(os.path.join('', tex[0]))
+        self.image = pygame.image.load(os.path.join('tex', tex[0]))
         self.image = pygame.transform.scale(self.image, (100, 100))
         self.sprite = pygame.sprite.Sprite()
-        self.sprite.image = pygame.image.load(os.path.join('', tex[0]))
+        self.sprite.image = pygame.image.load(os.path.join('tex', tex[0]))
         
         self.rect = self.sprite.image.get_rect()
 
@@ -24,19 +24,19 @@ class Creature(pygame.sprite.Sprite):
         self.x, self.y = x, y
         self.rect.x, self.rect.y = x, y
 
-class Armor:
+class Armor(pygame.sprite.Sprite):
     pass
 
-class Weapon:
+class Weapon(pygame.sprite.Sprite):
     pass
 
-class Arch:
+class Arch(pygame.sprite.Sprite):
     pass
 
-class Magic:
+class Magic(pygame.sprite.Sprite):
     pass
 
-class Potion:
+class Potion(pygame.sprite.Sprite):
     pass
 
 class Enemy(Creature):
@@ -45,29 +45,83 @@ class Enemy(Creature):
 class NPC(Creature):
     pass
 
-class House:
-    pass
-
-class Thing:
+class Thing(pygame.sprite.Sprite):
     pass
 
 class Container(Thing):
     pass
 
-class Book():
+class Book(pygame.sprite.Sprite):
+    pass
+
+class Hero(Creature):
+    pass
+
+class Surface_Tile(pygame.sprite.Sprite):
+    pass
+
+class House(pygame.sprite.Sprite):
+    pass
+
+class Tree(pygame.sprite.Sprite):
+    pass
+
+class Bush(pygame.sprite.Sprite):
     pass
 
 
+def map(place):
+    pass
 
-game = True
-clock = pygame.time.Clock()
-gui = GUI()
-               
-fps = 60
-a = []
-ss = pygame.sprite.Group()
-sack = Creature(1, 2, 3, 4, 5, [], ['sack.png'], 0, 0, ss)
-while game:
+def quest_list():
+    pass
+
+def state():
+    pass
+
+def attack():
+    pass
+
+def start_screen():
+    global screen
+    global game
+    global intro
+    
+    screen.fill(pygame.Color('brown'))
+    gui.add_element('GUI_Label_Countable', 'name', [(200, 100), (400, 200), (0, 0)], 'GAME', None, (255, 255, 255), 40, 'limit_size', 'font_adapt')
+    gui.add_element('GUI_Button', 'new_game', ((200, 300), (400, 100)), new_game, 'black', 'grey', 'white', ['rect', 'New game', (255, 255, 255)])    
+    gui.add_element('GUI_Button', 'load_game', ((200, 420), (400, 100)), load_game, 'black', 'grey', 'white', ['rect', 'Load game', (255, 255, 255)])
+    gui.add_element('GUI_Button', 'exit_game', ((200, 540), (400, 100)), exit_game, 'black', 'grey', 'white', ['rect', 'Exit game', (255, 255, 255)])
+    
+    while game or intro:
+        process(screen)
+
+
+def pause():
+    pass
+
+def save_game():
+    pass
+
+def load_game(par):
+    global game
+    game = False
+
+def exit_game(par):
+    global game
+    global intro
+    
+    game = False
+    intro = False
+
+def new_game(par):
+    global game
+    intro = False
+
+def process(screen):
+    global game
+    global fps
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
@@ -76,22 +130,20 @@ while game:
                 if gui.active_element is not None and not (gui.active_element[0][0] <= event.pos[0] <= gui.active_element[0][0]+gui.active_element[1][0] and gui.active_element[0][1] <= event.pos[1] <= gui.active_element[0][1]+gui.active_element[1][1]):
                     if hasattr(gui.elements[gui.active_element], 'state'):
                         gui.elements[gui.active_element].state = 'normal'
-                    gui.active_element = None if not gui.blocked else gui.active_element
+                    gui.active_element = None
                 if i[0][0] <= event.pos[0] <= i[0][0]+i[1][0] and i[0][1] <= event.pos[1] <= i[0][1]+i[1][1]:
                     gui.active_element = i
                     if hasattr(gui.elements[i], 'state'):
                         gui.elements[i].state = 'selected'
-            
+
         if event.type in [pygame.MOUSEBUTTONDOWN, pygame.MOUSEBUTTONUP]:
             for i in gui.elements.keys():
                 if gui.active_element is not None and not (gui.active_element[0][0] <= event.pos[0] <= gui.active_element[0][0]+gui.active_element[1][0] and gui.active_element[0][1] <= event.pos[1] <= gui.active_element[0][1]+gui.active_element[1][1]):
                     if hasattr(gui.elements[gui.active_element], 'state'):
                         gui.elements[gui.active_element].state = 'normal'
                     gui.active_element = None
-                    gui.blocked = False
                 if i[0][0] <= event.pos[0] <= i[0][0]+i[1][0] and i[0][1] <= event.pos[1] <= i[0][1]+i[1][1]:
                     if hasattr(gui.elements[i], 'state'):
-                        gui.blocked = True
                         gui.active_element = i
                         gui.elements[i].state = 'clicked'
                         gui.elements[i].func(event)
@@ -99,13 +151,18 @@ while game:
         if event.type == pygame.KEYDOWN:
             if gui.blocked:
                 if hasattr(gui.elements[gui.active_element],'texting'):
-                     gui.elements[gui.active_element].texting(event)
-            
-    screen.fill(pygame.Color('black'))
-    gui.render()
-    ss.draw(screen)
+                        gui.elements[gui.active_element].texting(event)
 
+    gui.render(screen)
     pygame.display.flip()
     clock.tick(fps)
 
+
+game = False
+clock = pygame.time.Clock()
+gui = GUI()      
+fps = 60
+
+intro = True
+start_screen()
 pygame.quit()
